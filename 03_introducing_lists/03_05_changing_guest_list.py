ac5713b6c41e520e93d7d9a01c34d74fffa4bf28
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-04 09:42:22 -0400

    9/4/2024

--- 03_introducing_lists/03_05_changing_guest_list.py
+++ 03_introducing_lists/03_05_changing_guest_list.py
@@ -14,7 +14,3 @@ guests.insert(0, "Mark Fischbach")
 for x in range (len(guests)):
     print("Hello, "+guests[x]+", I would like to invite you to dinner tonight to learn more about your life.")
 print("\n")
-
-
-
-
