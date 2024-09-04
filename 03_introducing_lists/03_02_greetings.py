484d4acbe7a9add0d22af92e66c12c365e306391
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-04 08:59:24 -0400

    9/3/2024

---
+++ 03_introducing_lists/03_02_greetings.py
@@ -0,0 +1,6 @@
+names = ["Josh","Peter","Max","Nathan J"]
+
+print()
+for x in range(len(names)):
+    print("Hello "+names[x]+", how are you today?")
+print()
---
+++ 03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,7 @@
+transportation = ["truck","plane","train","cars"]
+
+print()
+print("I own a "+transportation[0])
+print("I've never been on a "+ transportation[1] +" or a "+ transportation[2] + " before.")
+print("I don't know very much about "+transportation[3]+".")
+print()
