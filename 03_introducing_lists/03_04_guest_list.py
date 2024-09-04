50a6aa2b4ecb50e1eaacf342d7032093163ede00
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-04 09:40:24 -0400

    9/4/2024

---
+++ 03_introducing_lists/03_04_guest_list.py
@@ -0,0 +1,6 @@
+guests = ["Julius Caesar", "Hidetaka Miyazaki", "Albert Einstein"]
+
+print("\n")
+for x in range (len(guests)):
+    print("Hello, "+guests[x]+", I would like to invite you to dinner tonight to learn more about your life.")
+print("\n")
---
+++ 03_introducing_lists/03_05_changing_guest_list.py
@@ -0,0 +1,20 @@
+guests = ["Julius Caesar", "Hidetaka Miyazaki", "Albert Einstein"]
+
+print("\n")
+for x in range (len(guests)):
+    print("Hello, "+guests[x]+", I would like to invite you to dinner tonight to learn more about your life.")
+print("\n")
+
+print("Unfortunately, Julius Caesar could not make it to the dinner tonight.")
+print("\n")
+
+guests.pop(0)
+guests.insert(0, "Mark Fischbach")
+
+for x in range (len(guests)):
+    print("Hello, "+guests[x]+", I would like to invite you to dinner tonight to learn more about your life.")
+print("\n")
+
+
+
+
