d305a25a148bf111d215f163292db5ee58c2aff0
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-06 09:31:16 -0400

    9/6/2024

---
+++ 03_introducing_lists/03_09_dinner_guests.py
@@ -0,0 +1,8 @@
+guests = ["Julius Caesar", "Hidetaka Miyazaki", "Albert Einstein"]
+
+print("\n")
+for x in range (len(guests)):
+    print("Hello, "+guests[x]+", I would like to invite you to dinner tonight to learn more about your life.")
+print("\n")
+print("In total, there will be "+ str(len(guests)) +" invitees to dinner tonight.")
+print("\n")
