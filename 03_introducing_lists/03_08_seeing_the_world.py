537944447e2a6e0d76071a9027e6722229a582f9
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-06 09:25:13 -0400

    9/6/2024

---
+++ 03_introducing_lists/03_08_seeing_the_world.py
@@ -0,0 +1,42 @@
+locations = ["Japan", "France","Italy", "Germany","England"]
+print("\n")
+
+# original
+print(locations)
+print("\n")
+
+# sorted
+print(sorted(locations))
+print("\n")
+
+# original
+print(locations)
+print("\n")
+
+# sorted reverse
+print(sorted(locations, reverse = True))
+print("\n")
+
+# original
+print(locations)
+print("\n")
+
+# reverse
+locations.reverse()
+print(locations)
+print("\n")
+
+# back to original
+locations.reverse()
+print(locations)
+print("\n")
+
+# perment sorting
+locations.sort()
+print(locations)
+print("\n")
+
+# sorting in reverse
+locations.sort(reverse = True)
+print(locations)
+print("\n")
