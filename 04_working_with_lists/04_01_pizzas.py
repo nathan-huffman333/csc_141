36ef64249b4bcafb020c4a9c5979a86bdb28db2c
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-10 08:44:47 -0400

    9/10/2024

---
+++ 04_working_with_lists/04_01_pizzas.py
@@ -0,0 +1,17 @@
+pizzas = ["pepperoni", "cheese", "stuffed crust"]
+
+print("\n")
+for x in pizzas:
+    print(x)
+print("\n")
+
+for x in pizzas:
+    print("I like " + x + " pizza.")
+print("\n")
+
+print("When I was younger, I only liked "+ pizzas[1] + " pizza.")
+print("Now, my all time favorite, even if it is simple, is "+ pizzas[0] + " pizza.")
+print("I've only had "+pizzas[2]+" pizza a few times, and while it is really good, it's a LOT of cheese.\n")
+
+
+print("I love pizza, I think I could probably eat it every day without complaining.\n")
