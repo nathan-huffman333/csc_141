4303671c03e5fb46d6b289d042e5895d01774144
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-10 08:59:58 -0400

    9/10/2024

---
+++ 04_working_with_lists/04_02_animals.py
@@ -0,0 +1,12 @@
+animals = ["dog","cat","hamster"]
+
+print("\n")
+for x in animals:
+    print(x)
+print("\n")
+
+print("I have 2 "+animals[0]+"s.")
+print("I have 1 "+animals[1]+".")
+print("My sister has had 4 "+animals[2]+"s.\n")
+
+print("All of these animals make great pets, have four legs, and me or a family member has or has owned.\n")
