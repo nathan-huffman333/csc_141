80bb5daebf33df1ed10110860aa52d270e3f7f2a
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-12 08:10:47 -0400

    9/12/2024

---
+++ 04_working_with_lists/04_10_slices.py
@@ -0,0 +1,13 @@
+cubes = [value**3 for value in range(1, 11)]
+
+print("The first three items in the list are: ")
+for x in cubes[:3]:
+    print(x)
+
+print("\nThree items from the middle of the list are: ")
+for x in cubes[4 : -3]:
+    print(x)
+
+print("\nThe last three items in the list are: ")
+for x in cubes[-3:]:
+    print(x)
