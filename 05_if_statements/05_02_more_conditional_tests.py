997e1eeb32a3fa3503b7e78a287e312aebbf9163
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-16 09:44:25 -0400

    9/16/2024

---
+++ 05_if_statements/05_02_more_conditional_tests.py
@@ -0,0 +1,36 @@
+string_one = "Thing"
+string_two = "Other"
+
+print(string_one == string_two)
+print(string_one != string_two)
+print("\n")
+
+print(string_one.lower() == "thing")
+print(string_two.lower() == "OTHER")
+print("\n")
+
+num1 = 5
+num2 = 10
+
+print(num1*2 == num2)
+print(num1 == num2)
+print(num1 < num2)
+print(num1 > num2)
+print(num2 > num1)
+print(num2 < num1)
+print(num1*2 <= num2)
+print(num1*3 <= num2)
+print(num1 >= num2)
+print(num1*3 >= num2)
+print("\n")
+
+print(string_one == "Thing" and num1 == 5)
+print(string_two == "Thing" and num1 == 5)
+print(string_one == "Other" or num1 == 5)
+print(string_one == "Other" or num1 == 10)
+print("\n")
+
+list1 = ["one","two","three","four"]
+
+print("four" in list1)
+print("five" in list1)
