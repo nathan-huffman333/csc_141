a81633121ac7e608c2029e98f1ab84d5d7ecf2ff
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-17 08:29:20 -0400

    9/17/2024

---
+++ 05_if_statements/05_07_favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ["apple", "watermelon", "grapes"]
+
+if "apple" in favorite_fruits:
+    print("You like apples!")
+if "watermelon" in favorite_fruits:
+    print("You like watermelon!")
+if "grapes" in favorite_fruits:
+    print("You like grapes!")
+if "strawberry" in favorite_fruits:
+    print("You like strawberries!")
+if "orange" in favorite_fruits:
+    print("You like oranges!")
