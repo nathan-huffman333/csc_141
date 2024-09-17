3b5f970bae65289c030d6c5c3481a519c4f76483
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-17 10:05:18 -0400

    9/17/2024

---
+++ 05_if_statements/05_13_your_ideas.py
@@ -0,0 +1,9 @@
+"""
+As my programming skills continue to develop, there are many things that I'm interested in trying out.
+For example, now that I understand for loops, lists, and conditional statements, I'm thinking about how I could use them together.
+A list would be perfect for a video game inventory, and a for loop would be great for displaying the items in the inventory.
+Also, conditional statements could be used to trigger certain events depending on what is inside the inventory.
+Using this ability, things like allowing the player to open a door because they possess a key becomes possible.  
+Or, another things that lists and conditional statments could be used for is turn-based rpgs.
+If a list is filled with all of the commands a player can choose from, whenever they pick an action, an if statement can determine the outcome.
+"""
