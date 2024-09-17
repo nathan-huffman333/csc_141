ce7f73bd1a4a3845a8d880564b4590fcfbc5206f
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-17 08:21:04 -0400

    9/17/2024

---
+++ 05_if_statements/05_03_alien_colors_#1.py
@@ -0,0 +1,13 @@
+# This version fails
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("You have earned 5 points!")
+
+# this version succeeds
+
+if alien_color == "red":
+    print("You have earned 5 points!")
+
+
---
+++ 05_if_statements/05_06_stages_of_life.py
@@ -0,0 +1,14 @@
+age = 19
+
+if age < 2:
+    print("You are are a baby.")
+elif age >= 2 and age < 4:
+    print("You are are a toddler.")
+elif age >= 4 and age < 13:
+    print("You are are a kid.")
+elif age >= 13 and age < 20:
+    print("You are are a teenager.")
+elif age >= 20 and age < 65:
+    print("You are are an adult.")
+else:
+    print("You are are an elder.")
