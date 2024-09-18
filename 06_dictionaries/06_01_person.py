25addb54e2d361b7c6f8c0b4b1c2948566dfc184
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-18 09:42:18 -0400

    9/18/2024

---
+++ 06_dictionaries/06_01_person.py
@@ -0,0 +1,8 @@
+person = {'first_name': "Nathan", 'last_name': "Huffman", 'age': 19, 'city': "Doylestown"}
+
+print("\n")
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+print("\n")
---
+++ 06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'nathan': 333,
+    'anthony': 100,
+    'jack': 555,
+    'josh': 80,
+    'peter': 12
+}
+
+print("Nathan's favorite number is:",favorite_numbers['nathan'])
+print("Anthony's favorite number is:",favorite_numbers['anthony'])
+print("Jack's favorite number is:",favorite_numbers['jack'])
+print("Josh's favorite number is:",favorite_numbers['josh'])
+print("Peter's favorite number is:",favorite_numbers['peter'])
