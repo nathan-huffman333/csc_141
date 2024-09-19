25649186aca2c8b2e7a8f544a246bb68f4d333fe
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-19 08:59:47 -0400

    9/19/2024

---
+++ 06_dictionaries/06_05_rivers.py
@@ -0,0 +1,16 @@
+rivers = {
+    'Egypt': "the nile",
+    'The USA': "the mississippi",
+    'Brazil and Peru': "the amazon"
+}
+
+# prints the (river) runs through (country)
+for country, river in rivers.items():
+    print(f"\n{river.title()} river runs through {country}")
+# prints the countries
+for country in rivers.keys():
+    print(f"\n{country}")
+# prints the rivers
+for country, river in rivers.items():
+    print(f"\n{river.title()}")
+print("\n")
