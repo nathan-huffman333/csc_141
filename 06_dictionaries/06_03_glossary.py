91df46e1656033236a8bb5ef9c05e4fb78f8e9ce
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-19 08:24:09 -0400

    9/19/2024

---
+++ 06_dictionaries/06_03_glossary.py
@@ -0,0 +1,14 @@
+glossary = {
+    'condition statement': "A command that will check to see if a specification is met, and if so, perform an action.",
+    'dictionary': "A collection of key-value pairs, with each key connected to a value and can be used to find the value related to it.",
+    'key-value pair': "A set of values associated with each other. When you provide a key, it will return the value associated with it.",
+    'list': "A type of variable that can store information which can be added to or removed",
+    'tuple': "A type of list in python that cannot have items removed or add to."
+}
+
+print("\n")
+print("Condition statement: ",glossary['condition statement'],"\n")
+print("Dictionary: ",glossary['dictionary'],"\n")
+print("Key-value pair: ",glossary['key-value pair'],"\n")
+print("List: ",glossary['list'],"\n")
+print("Tuple: ",glossary['tuple'],"\n")
