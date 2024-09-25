5c248ec1a789f4992dc0cd49b32900f5c64f3bb9
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-25 11:08:46 -0400

    9/25/2024

--- 06_dictionaries/06_11_cities.py
+++ 06_dictionaries/06_11_cities.py
@@ -2,11 +2,11 @@ cities = {
     'New York City' : {
         'Country' : "USA",
         'Population' : "8 Million",
-        "Fun Fact" : "Home to the 'whispering gallery', where people can whisper from across the room and still hear each other."
+        'Fun Fact' : "Home to the 'whispering gallery', where people can whisper from across the room and still hear each other."
+    },
+    'Tokyo' : {
+        'Country' : "Japan",
+        'Population' : "37 Million",
+        'Fun Fact' : "There are people called “Oshiyas” who's job is to push commuters into crowded trains during rush hour to fit inside."
     }
-
-    'Tokyo' :
-
-
-
 }
