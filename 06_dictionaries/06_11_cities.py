4d4b95cf14328816b19db5f898e2e72171659c22
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-25 11:40:29 -0400

    9/25/2024

--- 06_dictionaries/06_11_cities.py
+++ 06_dictionaries/06_11_cities.py
@@ -1,12 +1,27 @@
+# This code displays information about different cities.
+
 cities = {
-    'New York City' : {
+    'New York City:' : {
         'Country' : "USA",
         'Population' : "8 Million",
-        'Fun Fact' : "Home to the 'whispering gallery', where people can whisper from across the room and still hear each other."
+        'Fact' : "Home to the 'whispering gallery', where people can whisper from across the room and still hear each other.",
     },
-    'Tokyo' : {
+    
+    'Tokyo:' : {
         'Country' : "Japan",
         'Population' : "37 Million",
-        'Fun Fact' : "There are people called “Oshiyas” who's job is to push commuters into crowded trains during rush hour to fit inside."
+        'Fact' : "There are people called “Oshiyas” who's job is to push commuters into crowded trains during rush hour to fit inside.",
+    },
+
+    'Paris:' : {
+        'Country' : "France",
+        'Population' : "11 Million",
+        'Fact' : "Originally, the Eiffel Tower was meant to be a temporary monument to demonstrate superior construction skills and technology",
     }
 }
+
+for city, info in cities.items():
+    print(f"\n{city}")
+    print(f"\tCountry: {info['Country']}")
+    print(f"\tPopulation: {info['Population']}")
+    print(f"\tFact: {info['Fact']}")
