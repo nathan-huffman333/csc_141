5038dd2c718774567acdc05afc687b5d8eb494b2
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-09-30 09:37:29 -0400

    9/30/2024

---
+++ 08_functions/08_01_message.py
@@ -0,0 +1,9 @@
+# This code is to display a short sentence about what we are learning in the chapter.
+
+import os
+os.system('cls')
+
+def display_message():
+    print("In this chapter, we will learn about functions, which are named blocks of code designed to do one specific job.")
+
+display_message()
