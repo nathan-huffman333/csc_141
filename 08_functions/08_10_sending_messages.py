6fe7a0906d747e999072a7d9e722eac8f3040a8a
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-10-03 08:13:00 -0400

    10/3/2024

---
+++ 08_functions/08_10_sending_messages.py
@@ -0,0 +1,20 @@
+# This code shows text messages and moves the messages from one list to another individually.
+
+import os
+os.system('cls')
+
+def show_messages(joke):
+    for message in joke:
+        print(message)
+
+def send_messages(joke):
+    for x in joke:
+        print(x)
+        sent_messages.append(x)
+
+sent_messages = []
+joke = ["knock knock.", "who's there?", "Kenya", "Kenya who?", "Kenya lend me some money?"]
+
+send_messages(joke)
+print(f"\nFirst list:{joke}")
+print(f"Second list: {sent_messages}")
