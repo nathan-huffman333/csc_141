0a5ff30f0e5502b65d93cc11b2e26a1f231657ea
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-10-24 08:18:54 -0400

    10/24/2024

---
+++ 09_classes/09_02_three_restaurants.py
@@ -0,0 +1,31 @@
+# This code describes the attributes of three different restaurants by using a class.
+
+import os
+os.system('cls')
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"Cuisine Type: {self.cuisine_type}")
+        return self
+    
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+        return self
+
+
+restaurant = Restaurant("La Bella Italia", "Italian")
+restaurant.describe_restaurant()
+print("\n")
+
+restaurant = Restaurant("Wendy's", "Fast Food")
+restaurant.describe_restaurant()
+print("\n")
+
+restaurant = Restaurant("Ramsay's Kitchen", "Fine Dining")
+restaurant.describe_restaurant()
+print("\n")
