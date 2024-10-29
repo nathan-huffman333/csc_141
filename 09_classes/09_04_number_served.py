e6bb91bffbe5d8040e5c02d8ff8b0ea13a0b3af6
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-10-29 08:53:45 -0400

    10/29/2024

--- 09_classes/09_04_number_served.py
+++ 09_classes/09_04_number_served.py
@@ -1,11 +1,13 @@
+# This code is able to print and update the number of customers a restaurant serves by using different methods.  
+
 import os
 os.system('cls')
 
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type, number_served = 0):
+    def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        self.number_served = number_served
+        self.number_served = 0
         
     def describe_restaurant(self):
         print(f"Restaurant Name: {self.restaurant_name}")
@@ -17,18 +19,25 @@ class Restaurant:
         print(f"{self.restaurant_name} is now open!")
 
     
-    def set_number_served(self):
-        customers = input("Customers served: ")
-        number_served = customers
-        return(number_served)
-    
-    def increment_number_served(self):
-        print()
+    def set_number_served(self, number_served):
+        self.number_served = number_served
 
-restaurant = Restaurant("La Bella Italia", "Italian", 25)
+    
+    def increment_number_served(self, additional_served):
+        self.number_served += additional_served
+
+restaurant = Restaurant("La Bella Italia", "Italian")
 restaurant.describe_restaurant()
 print("\n")
-restaurant = Restaurant("La Bella Italia", "Italian", 17)
+
+restaurant.set_number_served(50)
 restaurant.describe_restaurant()
 print("\n")
-restaurant.set_number_served()
+
+restaurant.set_number_served(250)
+restaurant.describe_restaurant()
+print("\n")
+
+restaurant.increment_number_served(50)
+restaurant.describe_restaurant()
+print("\n")
