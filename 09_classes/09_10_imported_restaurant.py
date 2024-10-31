e33b728fde979dba0668ad0e3045882d8856cdd9
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-10-31 08:38:15 -0400

    10/31/2024

---
+++ 09_classes/09_10_imported_restaurant.py
@@ -0,0 +1,31 @@
+# This code uses a subclass for specifically Ice Cream stands in the parent class Restaurant.
+
+import os
+os.system('cls')
+from restaurant import Restaurant
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["vanilla", "chocolate", "peanut butter cup", "mint chocolate chip", "cotton candy"]
+
+    def display_flavors(self):
+        print("Flavors Available:")
+        for x in self.flavors:
+            print(f"\t{x.title()}")
+
+
+
+restaurant = IceCreamStand("Joe's Ice Cream Shop", "Ice Cream")
+restaurant.describe_restaurant()
+print("")
+restaurant.display_flavors()
+print("")
+restaurant.open_restaurant()
+print("\n")
+
+restaurant = Restaurant("Ramsay's Kitchen", "Fine Dining")
+restaurant.describe_restaurant()
+print("\n")
+restaurant.open_restaurant()
+print("\n")
