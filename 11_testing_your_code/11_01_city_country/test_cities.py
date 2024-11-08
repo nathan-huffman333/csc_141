dda08bead55d26d812b38082c261bf39c3ac28bc
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-11-08 17:39:43 -0500

    11/8/2024

---
+++ 11_testing_your_code/11_01_city_country/test_cities.py
@@ -0,0 +1,8 @@
+# Difficulty: 9/10 I could not for the life of me figure out how to set up pytest correctly, the code itself was incredibly easy, but for some reason pytest just wasn't working for a long time.
+# This code takes a function from another file and using another function, runs it to create a formated location of a city and country.
+
+from city_functions import city_country
+
+def test_city_country():
+    formatted = city_country("new york city", "the united states of america")
+    print(f"\tNeatly formatted location: {formatted}")
