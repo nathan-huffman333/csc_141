6ab6c9822aa3fe8eb9e1189900dc1a6c1131fe93
nathan-huffman333 <nathan.huffman001@albright.edu>
2024-11-08 17:40:39 -0500

    11/8/2024

--- 11_testing_your_code/11_01_city_country/test_cities.py
+++ 11_testing_your_code/11_01_city_country/test_cities.py
@@ -2,6 +2,8 @@
 # This code takes a function from another file and using another function, runs it to create a formated location of a city and country.
 
 from city_functions import city_country
+import os
+os.system('cls')
 
 def test_city_country():
     formatted = city_country("new york city", "the united states of america")
